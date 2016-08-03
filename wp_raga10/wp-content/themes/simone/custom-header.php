2b73589f5d9bc781a8574373f815a6422df010eb
Radeox <dawid.weglarz95@gmail.com>
2016-08-03 14:44:59 +0200

    Added credits in footer and save copies of changes as custom-*
    Just in case of future upgrades

--- wp_raga10/wp-content/themes/simone/custom-header.php
+++ wp_raga10/wp-content/themes/simone/custom-header.php
@@ -9,6 +9,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<title>Raga10</title>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
